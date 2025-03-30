05bde9167c3562ba6576def5484f39a858376638
rameshwor <nepalrameshwor011@gmail.com>
2025-03-30 15:41:16 +0545

    package and image update

--- src/app/TokenProvider.tsx
+++ src/app/TokenProvider.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { useVerifyTokenMutation } from '@/redux/features/auth/authApi';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
 
 interface propsI {
     children: React.ReactNode;
 }
 const TokenProvider = (props: propsI) => {
     // const pathname = window.location.pathname;
+    const router = useRouter();
     const [pathname, setPathname] = useState<string | null>(null);
     const [verifyToken] = useVerifyTokenMutation();
-
     useEffect(() => {
         if (typeof window !== "undefined") {
             setPathname(window.location.pathname);
@@ -17,12 +19,18 @@ const TokenProvider = (props: propsI) => {
         const authToken = localStorage.getItem("msp_auth_token") || "";
 
         const verifyUserToken = async () => {
-            await verifyToken({ refresh: authToken });
+            await verifyToken({ refresh: authToken })
+                .unwrap()
+                .then(() => { })
+                .catch(() => {
+                    router.push('/login')
+                    toast.error("Failed to validate token")
+                })
         };
 
-        if (pathname !== "/login" && pathname !== "/signup") {
-            verifyUserToken();
-        }
+        // if (pathname !== "/login" && pathname !== "/signup") {
+        //     verifyUserToken();
+        // }
     }, [verifyToken, pathname]);
     return (
         <div>  {props.children}</div>
